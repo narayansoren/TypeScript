d3de596e06d07250015612bc9fe250d6fb32e331
itz-XD <mi125g8256@gmail.com>
2026-03-03 13:07:16 +0530

    add type narrowing functions and chai order handling

---
+++ Basic/src/typeNarrowing.ts
@@ -0,0 +1,90 @@
+function getChai(kind: string | number) {
+  if (typeof kind === "string") {
+    return `Making ${kind}  chai...`;
+  }
+  return `Chai order: ${kind}`;
+}
+
+function serveChai(msg?: string) {
+  if (msg) {
+    return `Serving ${msg}`;
+  }
+  return `Serving default masala chai`;
+}
+
+function orderChai(size: "small" | "medium" | "large" | number) {
+  if (size === "small") {
+    return `small cutting chai...`;
+  }
+  if (size === "medium" || size === "large") {
+    return `make extra chai`;
+  }
+  return `chai order ${size}`;
+}
+
+class KulhadChai {
+  serve() {
+    String`Serving Kulhad Chai`;
+  }
+}
+
+class Cutting {
+  serve() {
+    String`Serving Kulhad Chai`;
+  }
+}
+
+function serve(chai: KulhadChai | Cutting) {
+  if (chai instanceof KulhadChai) {
+    return chai.serve();
+  }
+}
+
+type ChaiOrder = {
+  type: string;
+  sugar: number;
+};
+
+function isChaiOrder(obj: any): obj is ChaiOrder {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    typeof obj.type === "string" &&
+    typeof obj.sugar === "number"
+  );
+}
+
+function serveOrder(item: ChaiOrder | string) {
+  if (isChaiOrder(item)) {
+    return `Serving ${item.type} chai with ${item.sugar}`;
+  }
+  return `Serving custom chai: ${item}`;
+}
+
+type MasalaChai = { type: "masala"; spicelevel: number };
+type GingerChai = { type: "ginger"; amount: number };
+type ElaichiChai = { type: "elaichi"; aroma: number };
+
+type Chai = MasalaChai | GingerChai | ElaichiChai;
+
+function MakeChai(order: Chai) {
+  switch (order.type) {
+    case "masala":
+      return `Masala chai`;
+      break;
+    case "elaichi":
+      return `Elaichi chai`;
+      break;
+    case "ginger":
+      return `Ginger chai`;
+      break;
+  }
+}
+
+function brew(order: MasalaChai | GingerChai) {
+  if ("spicelevel" in order) {
+    //
+  }
+}
+
+// function isStringArray(arr: unknown): arr is string[] {}
